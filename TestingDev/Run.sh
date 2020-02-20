d1416752f9b14952b9ead9acceb0c4ccaf044346
OMKAR DATTATRAY MORE <51694363+Omkarmore29@users.noreply.github.com>
2020-02-20 14:55:34 +0530

    Delete Run.sh

--- TestingDev/Run.sh
+++
@@ -1,4 +0,0 @@
-cd /var/lib/jenkins/workspace/Demo/TestingDev/
-set path="/var/lib/jenkins/workspace/Demo/TestingDev/"
-sudo javac -cp ".:/var/lib/jenkins/workspace/Demo/TestingDev/lib/*:/var/lib/jenkins/workspace/Demo/TestingDev/bin:" src/Testng/NewTest.java
-java -cp ".:/var/lib/jenkins/workspace/Demo/TestingDev/lib/*:/var/lib/jenkins/workspace/Demo/TestingDev/bin:" org.testng.TestNG testng.xml
